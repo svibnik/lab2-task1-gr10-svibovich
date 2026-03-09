d99b7727843fd13eadbf4d61a59caedbb9a1c963
svibnik <svibovichnikita444@gmail.com>
2026-03-09 20:36:47 +0000

    Add function to check lucky numbers

--- src/main.c
+++ src/main.c
@@ -1,8 +1,33 @@
-/* hello.c */
 #include <stdio.h>
 
-void main (void)
-{
-	printf ("Hello World\n");
-	printf ("Goodbye World\n");
+/*
+ * Автор: Свибович Никита, группа 10
+ * Задание: поиск всех шестизначных счастливых номеров
+ */
+
+/**
+ * Проверяет, является ли шестизначное число "счастливым"
+ * @param number шестизначное число
+ * @return 1 если счастливое, 0 если нет
+ */
+int is_lucky(int number) {
+    int digits[6];
+    int temp = number;
+    
+    // Разбираем число на цифры
+    for (int i = 5; i >= 0; i--) {
+        digits[i] = temp % 10;
+        temp /= 10;
+    }
+    
+    // Сумма первых трёх цифр
+    int sum1 = digits[0] + digits[1] + digits[2];
+    // Сумма последних трёх цифр
+    int sum2 = digits[3] + digits[4] + digits[5];
+    
+    return sum1 == sum2;
+}
+
+int main() {
+    return 0;
 }
